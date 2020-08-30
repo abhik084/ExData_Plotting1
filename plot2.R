45f8ec561a33bdf3cd013ecbcece4ce01dec30ea
ABHIK PATRA <66292542+abhik084@users.noreply.github.com>
2020-08-30 20:30:54 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,13 +1,35 @@
-PowerCons <- read.table("household_power_consumption.txt",sep = ";",header = TRUE)
+# Scripts for Getting and Cleaning Data course project
 
-PowerCons$DateTime <- as.POSIXct(paste(PowerCons$Date, PowerCons$Time),format = "%d/%m/%Y %H:%M:%S")
-PowerCons$Date <- as.Date(PowerCons$Date, format = "%d/%m/%Y")
-Data<- subset(PowerCons, ((Date == as.Date("2007-02-01")) | (Date == as.Date("2007-02-02"))) )
+##loading libraries
+
+library(dplyr)
+library(stringr)
+library(reshape2)
+library(chron)
+library(data.table)
+##-----------------------------------FILE PREPARATION------------------------------------
+
+##!!!!!!!!!!!!!!!!must enter the path for the directory with unzipped data files!!!!!!!!!
+
+## Directory path for data files
+DirPath <- '<your path to the file (including the file & extension)>' 
 
 
-Data$Global_active_power <- as.numeric(Data$Global_active_power)
+##--------------------------READING AND CONVERTING DATA----------------------------------
+## Reading household power consumption (HPC) file
+HPC_data <- read.table(file = DirPath, sep = ';', header = TRUE)
+## Converting Date and Time columns
+HPC_data$Date <- as.Date.character(HPC_data$Date, '%d/%m/%Y')
+HPC_data$Time <- times(HPC_data$Time)
+## Subsetting values for 2007-02-01 and 2007-02-02
+HPC_dt <- subset(HPC_data, Date == '2007-02-01'| Date == '2007-02-02')
+## Converting numeric columns
+HPC_dt[, 3:9] <- sapply(HPC_dt[, 3:9], as.numeric)
+## Creating Date + Time column
+Date_Time <- strptime(paste(HPC_dt$Date, HPC_dt$Time, sep=" "), '%Y-%m-%d %H:%M:%S')
+HPC_dt <- cbind(HPC_dt, Date_Time)
 
-plot(Data$DateTime, Data$Global_active_power, type = "l", ylab = "GLobal Active Power(kilowatts)", xlab  ="")
-
-dev.copy(png, 'plot2.png',height = 480, width = 480)
-dev.off
+##-----------------------------------PLOTTING-------------------------------------------
+png("plot2.png", width=480, height=480)
+with(HPC_dt, plot(Date_Time, Global_active_power, type = 'l', xlab = 'Day', ylab="Global Active Power (kilowatts)"))
+dev.off()
